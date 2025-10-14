206d94ee94fb51b9897fb4ea126c83fb9ca3cd73
Bencevrga <2005bencevarga@gmail.com>
2025-10-14 19:06:24 +0200

    Fix Astro site URL for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://<felhasznaloneved>.github.io',
-  base: '/katica-interiors/',
+  site: 'https://github.com/Bencevrga/katicainteriors.git',
+  base: '/katicainteriors/',
   outDir: 'dist',
 });
